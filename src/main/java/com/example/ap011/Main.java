1e11d552efe2ac544629dc720e018634c584c82b
yarahmadi0077 <yarahmadi0077@gmail.com>
2024-06-24 13:37:58 +0330

    commit

--- src/main/java/com/example/ap011/Main.java
+++ src/main/java/com/example/ap011/Main.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-//
+
 public class Main extends Application {
 
     @Override
